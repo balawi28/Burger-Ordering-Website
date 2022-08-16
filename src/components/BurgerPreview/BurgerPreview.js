54c68361b9b92868ec3b44c9717bdab7b51f8e0c
Mohamad Balawi <malek22139@gmail.com>
2022-08-17 00:10:24 +0300

    16-8-2022-very-late

--- src/components/BurgerPreview/BurgerPreview.js
+++ src/components/BurgerPreview/BurgerPreview.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import BurgerBreadBottom from '../../icons/burger-bread-bottom.svg';
-import BurgerBreadTop from '../../icons/burger-bread-top.svg';
-import BurgerMeatBeef from '../../icons/burger-meat-beef.svg';
 import './BurgerPreview.css';
 
 export default function BurgerPreview({ elements }) {
   return (
     <div className='BurgerPreview'>
-      <img src={BurgerBreadBottom} alt='Bread: Base Price' />
-      <img src={BurgerMeatBeef} alt='Beef: Base Price' />
       {elements &&
         elements.map(({ image, text }) => (
           <img className='id' src={image} alt={text} />
         ))}
-      <img src={BurgerBreadTop} alt='Bread: Base Price' />
     </div>
   );
 }
