75de31ef7b322dbe5616a7ccc6d06fd20a01ecd6
Mohammed Balawi <MohammedBA@exalt.ps>
2022-08-16 16:38:30 +0300

    16-8-2022-last

---
+++ src/features/order.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+let id = 1;
+const initialState = {
+  orders: [],
+};
+
+export const orderSlice = createSlice({
+  name: 'order',
+  initialState,
+  reducers: {
+    addOrder: (state, { payload }) => {
+      state.orders = [
+        ...state.orders,
+        {
+          id: id++,
+          elements: payload.elements,
+          status: true,
+          price: payload.price,
+        },
+      ];
+    },
+
+    deleteOrder: (state, { payload }) => {
+      state.orders = state.orders.filter((order) => order.id !== payload);
+    },
+
+    cancelOrder: (state, { payload }) => {
+      state.orders = state.orders.map((order) => {
+        return order.id === payload ? { ...order, status: false } : order;
+      });
+    },
+  },
+});
+export const { addOrder, cancelOrder, deleteOrder } = orderSlice.actions;
+export default orderSlice.reducer;
