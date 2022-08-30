16c3b4d6d28ca7b7dd75e19d31b620ae549fd547
Mohammed Balawi <MohammedBA@exalt.ps>
2022-08-30 12:53:41 +0300

    30-8-2022 mid

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import burgerReducer from './features/burger';
-import discountReducer from './features/discount';
-import orderReducer from './features/order';
-
-const store = configureStore({
-  reducer: {
-    burger: burgerReducer,
-    order: orderReducer,
-    discount: discountReducer,
-  },
-});
+import AppWrapper from './AppWrapper';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <AppWrapper />
   </React.StrictMode>
 );
